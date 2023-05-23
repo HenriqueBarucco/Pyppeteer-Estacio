ca4d6da0a0c8ec2716bfbdb585768c67a86d4535
Henrique Barucco <henrique_barucco@hotmail.com>
2023-05-23 17:08:01 -0300

    feat(pyppeteer.py): adiciona importação do módulo xpath e substitui referências a xcode por xpath Adiciona a importação do módulo xpath e substitui todas as referências a xcode por xpath no arquivo pyppeteer.py. Essa mudança foi feita para melhorar a legibilidade do código e torná-lo mais fácil de entender.

--- src/helpers/pyppeteer.py
+++ src/helpers/pyppeteer.py
@@ -1,5 +1,5 @@
 from pyppeteer import launch
-import src.entities.xcode as xcode
+import entities.xpath as xpath
 import asyncio
 import requests
 
@@ -12,31 +12,31 @@ class NotSelenium:
 
     async def login(self, email, password):
         # Esperando o botão de login aparecer
-        await self._waitAndClick(xcode.LOGIN_BUTTON)
+        await self._waitAndClick(xpath.LOGIN_BUTTON)
         await self.__page.screenshot({'path': 'screenshots/1-login.png'})
         print('Botão de login clicado!')
 
         # Selecionar o campo de email
-        await self._waitAndType(xcode.EMAIL_FORM, email)
+        await self._waitAndType(xpath.EMAIL_FORM, email)
         await self.__page.screenshot({'path': 'screenshots/2-email.png'})
         print('Email digitado!')
 
         # Selecionar o botão de "Próximo"
-        await self._waitAndClick(xcode.NEXT_BUTTON)
+        await self._waitAndClick(xpath.NEXT_BUTTON)
         print('Botão de próximo clicado!')
 
         # Selecionar o campo de senha
-        await self._waitAndType(xcode.PASSWORD_FORM, password)
+        await self._waitAndType(xpath.PASSWORD_FORM, password)
         await self.__page.screenshot({'path': 'screenshots/3-senha.png'})
         print('Senha digitada!')
 
         # Selecionar o botão de "Entrar"
-        await self._waitAndClick(xcode.ENTER_BUTTON)
+        await self._waitAndClick(xpath.ENTER_BUTTON)
         await self.__page.screenshot({'path': 'screenshots/4-no.png'})
         print('Botão de não clicado!')
 
         # Selecionar o botão de "Não"
-        await self._waitAndClick(xcode.NO_BUTTON)
+        await self._waitAndClick(xpath.NO_BUTTON)
         await asyncio.sleep(5)
         await self.__page.screenshot({'path': 'screenshots/5-saladeaula.png'})
         print('Sala de Aula aberta!')
@@ -46,7 +46,7 @@ class NotSelenium:
         await self._openTema5()
 
         # Selecionar o arquivo para download (Grupo 1)
-        element = await self.__page.waitForXPath(xcode.GRUPO_1)
+        element = await self.__page.waitForXPath(xpath.GRUPO_1)
         await element.click()
         
         await self.__page.screenshot({'path': 'screenshots/8-baixar.png'})
@@ -67,12 +67,12 @@ class NotSelenium:
         
     async def _openTema5(self):
         # Selecionar Card da matéria (Paradigmas de Python...)
-        await self._waitAndClick(xcode.CARD_MATERIA)
+        await self._waitAndClick(xpath.CARD_MATERIA)
         await self.__page.screenshot({'path': 'screenshots/6-abrirmateria.png'})
         print('Matéria aberta!')
 
         # Selecionar o tema (Tema 5)
-        await self._waitAndClick(xcode.TEMA_5)
+        await self._waitAndClick(xpath.TEMA_5)
         await self.__page.screenshot({'path': 'screenshots/7-abrirtema.png'})
         print('Tema aberto!')
             
