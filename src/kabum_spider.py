5a080bf9944865661cc02a9c451374acdc65341e
Henrique Barucco <henrique_barucco@hotmail.com>
2023-05-25 14:12:22 -0300

    feat(kabum_spider.py): adiciona suporte para salvar dados em um arquivo xlsx fix(kabum_spider.py): corrige a URL de busca para incluir todos os resultados fix(kabum_spider.py): corrige a extração de preços normais O suporte para salvar dados em um arquivo xlsx foi adicionado para permitir que os dados coletados sejam facilmente acessíveis e compartilhados. A URL de busca foi corrigida para incluir todos os resultados, pois a URL anterior limitava a quantidade de resultados. A extração de preços normais foi corrigida para evitar erros de extração.

--- src/kabum_spider.py
+++ src/kabum_spider.py
@@ -1,33 +1,49 @@
 import scrapy
+import logging
 from scrapy import Request
+from urllib.parse import urljoin
 from src.entities.product import Product
+from src.helpers.save_data import save_data
 
 class KabumSpider(scrapy.Spider):
     name = 'kabum'
     
-    def __init__(self, produto='Placa de vídeo', *args, **kwargs):
+    logging.getLogger('scrapy').propagate = False
+    
+    produtos = list()
+    
+    def __init__(self, produto, *args, **kwargs):
         super(KabumSpider, self).__init__(*args, **kwargs)
         self.produto = produto
-        self.produtos = list()
-        self.start_urls = ['https://www.kabum.com.br/busca/%s?page_number=1&page_size=100&facet_filters=&sort=price' % produto]
+        self.start_urls = ['https://www.kabum.com.br/busca/%s' % produto]
         
     def start_requests(self):
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.0.0'}
         for url in self.start_urls:
             yield Request(url, headers=headers)
+        
+    def closed(self, spider):
+        print("banana")
+        save_data.productToXlsx(self.produtos,'/home/mauri/Área de Trabalho/teste')
             
     def parse(self, response):
-        pages = response.xpath('//*[@id="listing"]/div/div/div/div/div/main/div/a')
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.0.0'}
-    
+        # Construir a nova URL com os parâmetros desejados
+        
+        new_url = urljoin(response.url, f"?page_number=1&page_size=100&facet_filters=&sort=price")
+        yield response.follow(new_url, headers=headers)
+        
+        pages = response.xpath('//*[@id="listing"]/div/div/div/div/div/main/div/a')
+
         for page in pages:
             yield response.follow(page, headers=headers, callback=self.parse_item)
             #yield Request(page, callback=self.parse_item)
 
-        next_page_container = response.xpath('//*[@id="listingPagination"]/ul/li[13]/a')
-        if len(next_page_container) > 0:
-            yield response.follow(next_page_container[0], headers=headers, callback=self.parse)
-
+        #next_page_container = response.xpath('//*[@id="listingPagination"]/ul')
+        #button_href = response.xpath('//*[@id="listingPagination"]/ul/li[7]/a/@href').get()
+        #print("NEXT PAGE CONTAINER", len(next_page_container))
+        #if len(next_page_container) > 0:
+        #    yield response.follow(button_href, headers=headers, callback=self.parse)
 
     def parse_item(self, response):
         item = Product()
@@ -47,7 +63,17 @@ class KabumSpider(scrapy.Spider):
         
         item['duracao'] = response.xpath('//*[@id="cardAlertaOferta"]/div[1]/div/div/span').get()
         
-
+        """ precos = {}
+        
+        precos['preco_normal'] = response.xpath('//*[@id="blocoValores"]/div[3]/b/text()').re(r'\d*\.*\d+\,\d+')
+        precos['preco_desconto'] = response.xpath('//*[@id="blocoValores"]/div[2]/div[1]/h4/text()').re(r'\d*\.*\d+\,\d+')
+        
+        if len(precos['preco_normal']) > 0:
+            try:
+                item['preco_normal'] = response.xpath('//*[@id="blocoValores"]/div[2]/div[1]/h4/text()').extract()
+            except AttributeError:
+                self.logger.debug('Falha ao extrair preço normal em %s', response.url)
+                pass """
         precos = {
             'preco_normal': response.xpath('//*[@id="blocoValores"]/div[3]/b/text()').re(r'\d*\.*\d+\,\d+'),
             'preco_desconto': response.xpath('//*[@id="blocoValores"]/div[2]/div[1]/h4/text()').re(r'\d*\.*\d+\,\d+')
@@ -58,7 +84,12 @@ class KabumSpider(scrapy.Spider):
                 item[key] = float(precos[key][0].replace('.','').replace(',','.'))
             else:
                 self.logger.debug('{} not found on %s'.format(key), 
-                    response.url)  
+                    response.url)
+                
+        if precos['preco_normal'] is not float:
+            precos['preco_normal'] = 'NA'
+            print('preco normal nao encontrado')
         
         self.produtos.append(item)
+        print(item)
         yield item
--- test.py
+++ test.py
@@ -2,10 +2,11 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from src.kabum_spider import KabumSpider
 
-def run_spider():
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(KabumSpider)
+def run_spider(produto):
+    process = CrawlerProcess()
+    process.crawl(KabumSpider, produto=produto)
     process.start()
 
 if __name__ == "__main__":
-    run_spider()
+    nome_do_produto = "placa"
+    run_spider(produto=nome_do_produto)
